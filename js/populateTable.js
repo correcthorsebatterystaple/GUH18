afd7ba5866a6b679148f05e1ca4feecf55d492b4
BSalim <burhan.salman@gmail.com>
2018-11-11 05:50:57 +0000

    add methods to add remove and update table values

--- js/populateTable.js
+++ js/populateTable.js
@@ -1,16 +1,46 @@
-for(var i = 0; i < ownedMemes.length; i++) {
 
+function addToTable(meme){
   var Notd = document.createElement("td");
   var Titletd = document.createElement("td");
   var Linktd = document.createElement("td");
+  var link = document.createElement('a');
   var Valuetd = document.createElement("td");
-  var Memetr = document.creteElement("tr");
+  var Changetd = document.createElement("td");
+  var Memetr = document.createElement("tr");
 
-  Notd.innerHTML = i+1;
-  Titletd.innerHTML = ownedMemes[i].title;
-  Linktd.innerHTML = ownedMemes[i].permalink;
-  Valuetd.innerHTML = ownedMemes[i].getValue();
+  Memetr.id = "meme-"+meme.id;
+  link.href = "https://www.reddit.com"+meme.permalink;
+  link.innerHTML = "LINK";
+  Notd.innerHTML = meme.id;
+  Titletd.innerHTML = meme.title;
+  Linktd.appendChild(link);
+  Valuetd.innerHTML = meme.getValue().toFixed(4);
+  Changetd.innerHTML = meme.getChange();
 
-  document.getElementById("OwnedMemes").appendChild(Memetr);
-
+  Memetr.appendChild(Notd);
+  Memetr.appendChild(Titletd);
+  Memetr.appendChild(Linktd);
+  Memetr.appendChild(Valuetd);
+  Memetr.appendChild(Changetd);
+  document.getElementById("OwnedMemes").appendChild(Memetr); 
 }
+
+function removeFromTable(meme){
+  let query = "meme-"+meme.id;
+  let row = document.getElementById(query);
+  row.parentElement.removeChild(row);
+}
+
+function updateValuesTable(){
+  if (ownedMemes.length === 0) return "err | empty meme ownership";
+  //update each row in the table
+  for (let i = 0; i<ownedMemes.length; i++) {
+    removeFromTable(ownedMemes[i]);
+    ownedMemes[i].updateData();
+    addToTable(ownedMemes[i]);
+  }
+}
+
+// setInterval(updateValuesTable, 2000);
+
+
