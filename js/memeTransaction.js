3522af3f435ef2aa09291f5c2ad0334ffae867ec
BSalim <burhan.salman@gmail.com>
2018-11-11 01:10:48 +0000

    push currentMeme to owned memes once bought

--- js/memeTransaction.js
+++ js/memeTransaction.js
@@ -2,25 +2,25 @@
 let balance = 0;
 let value = 0;
 
-function buyMeme(meme) {
-    ownedMemes.push(meme);
-    let memePrice = meme.getValue();
-    balance -= memePrice;
-    value += memePrice;
+function buyMeme() {
+  ownedMemes.push(currentMeme);
+  let memePrice = currentMeme.getValue();
+  balance -= memePrice;
+  value += memePrice;
 }
 
 function sellMeme(meme) {
-    var index = ownedMemes.indexOf(meme);
-    if (index > -1) {
-        ownedMemes.splice(index, 1);
-    }
-    balance += meme.getValue();
-    value += memePrice;
+  var index = ownedMemes.indexOf(meme);
+  if (index > -1) {
+    ownedMemes.splice(index, 1);
+  }
+  balance += meme.getValue();
+  value += memePrice;
 }
 
 function updateValue() {
-    value = 0;
-    for (let i = 0; i<ownedMemes.length; i++) {
-        value += ownedMemes[i].getValue();
-    }
+  value = 0;
+  for (let i = 0; i<ownedMemes.length; i++) {
+    value += ownedMemes[i].getValue();
+  }
 }
