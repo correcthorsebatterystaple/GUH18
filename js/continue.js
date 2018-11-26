93545634add862b04aa037c0588914216b06708a
Burhanuddin Salim <burhanuddin.salim@student.manchester.ac.uk>
2018-11-26 14:33:26 +0000

    fix changing purchase cost on refresh, and comments

--- js/continue.js
+++ js/continue.js
@@ -1,8 +1,11 @@
+/* FUNCTIONS THAT SAVE GAME STATE AND ALLOW TO CONTINUE GAME */
+
+// reads the given porfolio and updates the game state to that portfolio
 function readMemePortfolio(portfolio) {
   updateBalance(portfolio.balance);
   let owned = [];
   portfolio.owned.forEach(meme => {
-    let input = new Meme(meme.title, meme.permalink, meme.imgURL, meme.time, meme.score,meme.id);
+    let input = new Meme(meme.title, meme.permalink, meme.imgURL, meme.time, meme.initialValue,meme.id);
     owned.push(input);
   });
   memeIDCount = portfolio.memeIDCount;
@@ -10,6 +13,7 @@ function readMemePortfolio(portfolio) {
   createValuesTable();
 }
 
+// saves the current portfolio of memes
 function saveMemePortfolio() {
   let portfolio = {};
   portfolio.balance = balance;
@@ -19,11 +23,12 @@ function saveMemePortfolio() {
   let strPortfolio = JSON.stringify(portfolio);
   if (typeof(Storage) !== "undefined") {
     localStorage.setItem("portfolio", strPortfolio);
-  } else return "Unable to find local storage\n";
+    alert('Portfolio saved');
+  } else alert("Unable to find local storage.\nPortfolio not saveed!");
   return strPortfolio;
 }
 
-//loads a previous meme portfolio
+//loads a previous meme portfolio if exists
 (function(){
   if (localStorage.getItem("portfolio")) {
     let portfolio = JSON.parse(localStorage.getItem("portfolio"));
@@ -31,6 +36,7 @@ function saveMemePortfolio() {
   }
 })();
 
+// restarts the game
 function restartGame() {
   localStorage.removeItem("portfolio");
   location.reload();
