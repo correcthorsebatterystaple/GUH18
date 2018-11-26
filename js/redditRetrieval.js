9a2c15204557457818f044661b00e8775fbc2295
Burhanuddin Salim <burhanuddin.salim@student.manchester.ac.uk>
2018-11-26 14:34:03 +0000

    change var to let, add comments

--- js/redditRetrieval.js
+++ js/redditRetrieval.js
@@ -1,19 +1,21 @@
-var newMemeSet;
-var ownedMemes = [];
-var currentMeme;
-var memeIDCount = 1;
+/* FUNCTIONS THAT RETRIEVE THE MEMES AND SCORES FROM REDDIT */
 
-var PURCHASE_FEE_RATIO = 1.15;
+let newMemeSet;
+let ownedMemes = [];
+let currentMeme;
+let memeIDCount = 1;
 
-var subredditList = ["memes", "historymemes", "comedycemetery", "dankmemes"]
-var sortTypes = ["rising", "new"];
+const PURCHASE_FEE_RATIO = 1.15;
 
-//refresh set of memes
+const subredditList = ["memes", "historymemes", "comedycemetery", "dankmemes"]
+const sortTypes = ["rising", "new"];
+
+// Load new set of memes to be displayed
 function reloadNewMemes() {
-    var s = document.createElement("script");
+    let s = document.createElement("script");
 
-    var sub = subredditList[Math.floor(Math.random()*subredditList.length)];
-    var sort = sortTypes[Math.floor(Math.random()*sortTypes.length)];
+    let sub = subredditList[Math.floor(Math.random()*subredditList.length)];
+    let sort = sortTypes[Math.floor(Math.random()*sortTypes.length)];
 
     s.src = "http://www.reddit.com/r/" + sub + "/" + sort + ".json?limit=200&amp;jsonp=getRandomMemeCallback";
 
@@ -21,7 +23,7 @@ function reloadNewMemes() {
     document.body.removeChild(s);
 }
 
-//get a random meme from the
+// callback for retriving random meme
 function getRandomMemeCallback(data) {
     if(newMemeSet == null) {
       newMemeSet = data;
@@ -37,18 +39,18 @@ function getRandomMemeCallback(data) {
 function getRandomMeme() {
     if(newMemeSet == null) return "err | meme set not initialised";
 
-    for(var i = 0; i < 200; i++) {
-        var index = Math.floor(Math.random() * newMemeSet.data.children.length);
-        var permalink = newMemeSet.data.children[index].data.permalink;
-        var title = newMemeSet.data.children[index].data.title;
-        var score = newMemeSet.data.children[index].data.score;
-        var imgUrl = newMemeSet.data.children[index].data.url;
-        var time = newMemeSet.data.children[index].data.created;
-        var id = memeIDCount++;
+    for(let i = 0; i < 200; i++) {
+        let index = Math.floor(Math.random() * newMemeSet.data.children.length);
+        let permalink = newMemeSet.data.children[index].data.permalink;
+        let title = newMemeSet.data.children[index].data.title;
+        let score = newMemeSet.data.children[index].data.score;
+        let imgUrl = newMemeSet.data.children[index].data.url;
+        let time = newMemeSet.data.children[index].data.created;
+        let id = memeIDCount++;
 
-        var m = new Meme(title, permalink, imgUrl, time, score, id);
+        let m = new Meme(title, permalink, imgUrl, time, score, id);
 
-        for(var j = 0; j < ownedMemes.length; j++) {
+        for(let j = 0; j < ownedMemes.length; j++) {
             if(ownedMemes[j].permalink == m.permalink) continue;
         }
 
@@ -63,11 +65,12 @@ function getRandomMeme() {
 setInterval(reloadNewMemes, 5000);
 reloadNewMemes();
 
+// callback for updating the meme
 function updateMemeCallback(data) {
-    var score = data[0].data.children[0].data.score;
-    var permalink = data[0].data.children[0].data.permalink;
+    let score = data[0].data.children[0].data.score;
+    let permalink = data[0].data.children[0].data.permalink;
 
-    for(var i = 0; i < ownedMemes.length; i++) {
+    for(let i = 0; i < ownedMemes.length; i++) {
         if(ownedMemes[i].permalink == permalink) {
             ownedMemes[i].prevScore = ownedMemes[i].score;
             ownedMemes[i].change = score - ownedMemes[i].prevScore;
@@ -77,19 +80,19 @@ function updateMemeCallback(data) {
     }
 }
 
+// updates the meme that is given to the function
 function updateMemeData(meme) {
-    var s = document.createElement("script");
+    let s = document.createElement("script");
     s.src = "http://www.reddit.com" + meme.permalink + ".json?jsonp=updateMemeCallback";
     document.body.appendChild(s);
     document.body.removeChild(s);
 }
 
-// Note: when bought meme, add to ownedMemes array
-
+// update the graphics of the meme to display the meme on the screen
 function updateGraphics() {
     currentMeme = getRandomMeme();
     document.getElementById("memeTitle").innerHTML = currentMeme.title;
     document.getElementById("memeImage").src = currentMeme.imgURL;
     document.getElementById("memeScore").innerHTML = "Score: " + currentMeme.score;
-    document.getElementById("buyBtn").innerHTML = "$" + Math.floor(currentMeme.score * PURCHASE_FEE_RATIO);
+    document.getElementById("buyBtn").innerHTML = "$" + Math.floor(currentMeme.getPurchaseCost());
 }
